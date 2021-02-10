b2620aae31e86086a679fce93646bc504263fb57
unknown <calanhaga@aim.com>
2021-02-10 12:28:33 -0500

    MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+  const history = useHistory();
+
+const home = () => {
+  history.push('/');
+}
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
